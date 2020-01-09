3286ea9c704300937e5d90281cde5ca2bbc795d5
Mark Brazeau <kitsapbackflow@gmail.com>
2020-01-09 20:18:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -55,8 +55,14 @@ def reduce_to_all_true(source_array)
   end
 
   
-#def reduce_to_any_true(source_array)  
- 
+def reduce_to_any_true(source_array)  
+ i = 0
+  while i < source_array.length do
+   return false if !source_array[i]
+    i += 1
+  end
+    true
+  end
   
 
 
